006c97aeef747f92bbce2685f095dcba0f8cf9e4
bryanltobing <bryantobing0@gmail.com>
2024-02-04 22:39:10 +0800

    feat: alter table

--- db/schema.ts
+++ db/schema.ts
@@ -3,4 +3,5 @@ import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
 export const table = sqliteTable('table', {
   id: integer('id').primaryKey(),
   title: text("note"),
+  description: text("description")
 });
---
+++ drizzle/0001_fluffy_greymalkin.sql
@@ -0,0 +1,1 @@
+ALTER TABLE table ADD `description` text;
